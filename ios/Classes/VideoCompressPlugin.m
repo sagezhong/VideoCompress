5f8696e727df211e721bbca8d7471855547e846d
钟小强 <zhongxiaoqiang@inke.cn>
2020-12-10 16:58:29 +0800

    fix: swift header error

--- ios/Classes/VideoCompressPlugin.m
+++ ios/Classes/VideoCompressPlugin.m
@@ -1,5 +1,9 @@
 #import "VideoCompressPlugin.h"
+#if __has_include(<socket_link/socket_link-Swift.h>)
 #import <video_compress/video_compress-Swift.h>
+#else
+#import "video_compress-Swift.h"
+#endif
 
 @implementation VideoCompressPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
